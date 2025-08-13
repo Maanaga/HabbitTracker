2a3b22526a18dbf083fe18bada786c9477fa503d
Luka Managadze <managaluka0@gmail.com>
2025-08-13 22:08:03 +0400

    Adding weekly activities

---
+++ HabbitTracker/ShowHabbits.swift
@@ -0,0 +1,18 @@
+//
+//  ShowHabbits.swift
+//  HabbitTracker
+//
+//  Created by Luka Managadze on 8/13/25.
+//
+
+import SwiftUI
+
+struct ShowHabbits: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowHabbits()
+}
---
+++ HabbitTracker/WeeklyActive.swift
@@ -0,0 +1,18 @@
+//
+//  WeeklyActive.swift
+//  HabbitTracker
+//
+//  Created by Luka Managadze on 8/13/25.
+//
+
+import SwiftUI
+
+struct WeeklyActive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeeklyActive()
+}
