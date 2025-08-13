227c37e90d293f28c99a208aceb0ee15ae31e0a0
Luka Managadze <managaluka0@gmail.com>
2025-08-13 20:18:49 +0400

    Initial commit

--- HabbitTracker/ContentView.swift
+++ HabbitTracker/ContentView.swift
@@ -1,24 +1,93 @@
-//
-//  ContentView.swift
-//  HabbitTracker
-//
-//  Created by Luka Managadze on 8/12/25.
-//
-
 import SwiftUI
 
+let rows = [
+    GridItem(.adaptive(minimum: 200))
+    ]
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHGrid(rows: rows) {
+                    HStack(spacing: -5) {
+                        VStack {
+                            ZStack(alignment: .bottomLeading) {
+                                Image("Meditation")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: 150)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .padding()
+                                    .brightness(-0.2)
+                                VStack(alignment: .leading) {
+                                    Text("Meditaiton")
+                                        .foregroundStyle(.white)
+                                        .font(.title)
+                                    
+                                    
+                                    Text("Gain You Happiness And Piece")
+                                        .foregroundStyle(.white)
+                                        .font(.subheadline)
+                                    
+                                }
+                                .padding(30)
+                            }
+                        }
+                        VStack {
+                            ZStack(alignment: .bottomLeading) {
+                                Image("Learning")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: 150)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .padding()
+                                    .brightness(-0.1)
+                                VStack(alignment: .leading) {
+                                    Text("Education")
+                                        .foregroundStyle(.white)
+                                        .font(.title)
+                                    
+                                    
+                                    Text("Gain You Happiness And Piece")
+                                        .foregroundStyle(.white)
+                                        .font(.subheadline)
+                                    
+                                }
+                                .padding(30)
+                            }
+                        }
+                        VStack {
+                            ZStack(alignment: .bottomLeading) {
+                                Image("Travel")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: 150)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .padding()
+                                    .brightness(-0.2)
+                                VStack(alignment: .leading) {
+                                    Text("Travel")
+                                        .foregroundStyle(.white)
+                                        .font(.title)
+                                    
+                                    
+                                    Text("Gain You Happiness And Piece")
+                                        .foregroundStyle(.white)
+                                        .font(.subheadline)
+                                    
+                                }
+                                .padding(30)
+                            }
+                        }
+                    }
+                }
+            }
+        
         }
-        .padding()
+        .navigationTitle("HabbitTracker")
     }
 }
+    
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
