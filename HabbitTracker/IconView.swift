eab4df3fa3b48d746a578125713f1b898afdfbd8
Luka Managadze <managaluka0@gmail.com>
2025-08-13 20:10:37 +0400

    Initial commit

---
+++ HabbitTracker/IconView.swift
@@ -0,0 +1,18 @@
+//
+//  IconView.swift
+//  HabbitTracker
+//
+//  Created by Luka Managadze on 8/13/25.
+//
+
+import SwiftUI
+
+struct IconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconView()
+}
