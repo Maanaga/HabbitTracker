1eae559c4abe8c51512e000d01ecfa881ce30092
Luka Managadze <managaluka0@gmail.com>
2025-08-16 20:06:11 +0400

    half-full circle stroke

--- HabbitTracker/MeditationPage.swift
+++ HabbitTracker/MeditationPage.swift
@@ -7,11 +7,13 @@ struct MeditationPage: View {
         ScrollView {
             Spacer()
             Spacer()
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 15) {
                 Text("Meditation")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                    
+                    .padding(.top, 10)
+                    .padding(.horizontal, 20)
+                
                 ZStack {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
@@ -26,30 +28,116 @@ struct MeditationPage: View {
                         )
                         .frame(height: 120)
                     HStack {
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Meditation Progress")
-                            .foregroundStyle(.white)
-                            .font(.system(size: 25))
-                            .fontWeight(.semibold)
-                        Text("9 Exercise Left")
-                            .foregroundStyle(.secondary)
-                            .font(.system(size: 15))
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Meditation Progress")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 25))
+                                .fontWeight(.semibold)
+                            Text("9 Exercise Left")
+                                .foregroundStyle(.secondary)
+                                .font(.system(size: 15))
+                        }
+                        Spacer()
+                        ZStack {
+                            Circle()
+                                .trim(from: 0, to: 0.6)
+                                .rotation(Angle(degrees: -90))
+                                .stroke(Color.white, lineWidth: 5)
+                                .frame(width: 60, height: 60)
+                            
+                            Text("60%")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                        }
                     }
-                    Spacer()
-                    ZStack {
-                        Circle()
-                            .strokeBorder(Color.black.opacity(0.1), lineWidth: 1.0)
-                            .frame(width: 60, height: 60)
-                        
-                        Text("60%")
-                            .foregroundStyle(.white)
-                            .font(.headline)
-                    }
+                    .padding(.horizontal, 15)
                 }
-                .padding(.horizontal, 15)
+                .padding(.horizontal, 20)
+                
+                Text("Current Course")
+                    .font(.headline)
+                    .padding(.horizontal, 20)
+                
+                ZStack(alignment: .bottomLeading) {
+                    Image("MeditationTwo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 350, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .brightness(-0.1)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Beginner")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        
+                        Text("Take your first steps in meditation")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                    }
+                    .padding()
+                }
+                .padding(.horizontal, 30)
+                
+                
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(Color.black.opacity(0.2))
+                    .padding(.horizontal, 20)
+                
+                
+                Text("Other Courses")
+                    .font(.headline)
+                    .padding(.horizontal, 20)
+                
+                ZStack(alignment: .bottomLeading) {
+                    Image("MeditationThree")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 350, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .brightness(-0.1)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Intermediate")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        
+                        Text("Enhance your meditation skills")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                    }
+                    .padding()
+                }
+                .padding(.horizontal, 30)
             }
-        }
-            .padding(.horizontal)
+            
+            ZStack(alignment: .bottomLeading) {
+                Image("MeditationFour")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 350, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .brightness(-0.1)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Advanced")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    
+                    Text("Reach absolute inner piece")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                }
+                .padding()
+            }
+            .padding(.horizontal, 30)
         }
     }
 }
