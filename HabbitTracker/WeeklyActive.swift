e22e65c1b66d1459c507d9c48ac20f3208ddaf72
Luka Managadze <managaluka0@gmail.com>
2025-08-16 14:57:05 +0400

    Updating weekly active

--- HabbitTracker/WeeklyActive.swift
+++ HabbitTracker/WeeklyActive.swift
@@ -2,6 +2,7 @@
 import SwiftUI
 
 struct WeeklyActive: View {
+    @State private var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     var body: some View {
         VStack {
             ZStack {
@@ -16,29 +17,33 @@ struct WeeklyActive: View {
                         endPoint: .bottomTrailing
                     )
                 )
-                .frame(height: 120)
+                .frame(height: 140)
 
                             VStack(alignment: .leading, spacing: 15) {
                                 Text("Weekly Active Days")
                                     .font(.headline)
                                     .foregroundColor(.white)
                                 HStack(spacing: 8) {
-                                    ForEach(1..<8, id: \.self) { number in
-                                        ZStack {
-                                            Circle()
-                                                .fill(number == 1 ? Color.white : Color.white.opacity(0.3))
-                                                .frame(width: 40, height: 40)
+                                    ForEach(0..<7, id: \.self) { number in
+                                        VStack(spacing: 6) {
+                                            ZStack {
+                                                Circle()
+                                                    .fill(number == 1 ? Color.white : Color.white.opacity(0.3))
+                                                    .frame(width: 40, height: 40)
+                                                
+                                                Text("\(number + 1)")
+                                                    .foregroundStyle(number == 1 ? .green : .white)
+                                                    .font(.headline)
+                                            }
+                                            Text(days[number])
+                                                .font(.caption)
+                                                .foregroundColor(.secondary)
                                             
-                                            Text("\(number)")
-                                                .foregroundStyle(number == 1 ? .green : .white)
-                                                .font(.headline)
                                         }
                                     }
-                                    Spacer()
                                 }
                             }
-                                .padding()
-                        
+                            .padding()
             }
             .padding(.horizontal)
         }
