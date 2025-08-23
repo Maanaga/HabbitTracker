87923e1a4731933e6dce65341169287b14b99366
Luka Managadze <managaluka0@gmail.com>
2025-08-24 00:53:12 +0400

    adding forms and navigation destination

--- HabbitTracker/BeginnerPage.swift
+++ HabbitTracker/BeginnerPage.swift
@@ -1,25 +1,64 @@
 import SwiftUI
 
 struct BeginnerPage: View {
+    let meditations: [Meditation]
+    
+    var body: some View {
+        NavigationView {
+            List(meditations) { meditation in
+                NavigationLink(destination: MeditationDetailView(meditation: meditation)) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Step \(meditation.step)")
+                            .font(.caption)
+                        Text(meditation.title)
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.green)
+                        Text(meditation.description)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Beginner's Guide")
+        }
+    }
+}
+
+struct MeditationDetailView: View {
     let meditation: Meditation
+    
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 5) {
+            VStack(alignment: .leading, spacing: 6) {
                 Text("Step \(meditation.step)")
                     .font(.title)
                     .fontWeight(.semibold)
+                    .foregroundColor(.green)
+                
                 Text(meditation.title)
                     .font(.title2)
                     .fontWeight(.medium)
+                
                 Text(meditation.description)
                     .font(.headline)
+                    .foregroundStyle(.secondary)
+                
             }
+            .padding(.horizontal, 12)
         }
-       
+        .navigationTitle("Meditation")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    let meditations: [String: Meditation] = Bundle.main.decode("meditation.json")
-    return BeginnerPage(meditation: meditations["1"]!)
+    let allMeditations: [String: Meditation] = Bundle.main.decode("meditation.json")
+    
+    let beginnerMeditations = (1...10).compactMap { step in
+        allMeditations["\(step)"]
+    }
+    
+    return BeginnerPage(meditations: beginnerMeditations)
 }
--- HabbitTracker/MeditationPage.swift
+++ HabbitTracker/MeditationPage.swift
@@ -3,61 +3,123 @@
 import SwiftUI
 
 struct MeditationPage: View {
+    let allMeditations: [String: Meditation] = Bundle.main.decode("meditation.json")
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 15) {
-                Text("Meditation")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, 10)
-                    .padding(.horizontal, 20)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                    Color.green.opacity(0.9),
-                                    Color.green.opacity(0.6)
-                                ]),
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("Meditation")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top, 10)
+                        .padding(.horizontal, 20)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color.green.opacity(0.9),
+                                        Color.green.opacity(0.6)
+                                    ]),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing
+                                )
                             )
-                        )
-                        .frame(height: 120)
-                    HStack {
+                            .frame(height: 120)
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("Meditation Progress")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 25))
+                                    .fontWeight(.semibold)
+                                Text("9 Exercise Left")
+                                    .foregroundStyle(.secondary)
+                                    .font(.system(size: 15))
+                            }
+                            Spacer()
+                            ZStack {
+                                Circle()
+                                    .trim(from: 0, to: 0.6)
+                                    .rotation(Angle(degrees: -90))
+                                    .stroke(Color.white, lineWidth: 5)
+                                    .frame(width: 60, height: 60)
+                                
+                                Text("60%")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                            }
+                        }
+                        .padding(.horizontal, 15)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Text("Current Course")
+                        .font(.headline)
+                        .padding(.horizontal, 20)
+                    
+                    ZStack(alignment: .bottomLeading) {
+                        Image("MeditationTwo")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 350, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .brightness(-0.1)
+                        
+                        NavigationLink(destination: BeginnerPage(meditations: beginnerMeditations)) {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("Beginner")
+                                    .foregroundStyle(.white)
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                
+                                Text("Take your first steps in meditation")
+                                    .foregroundStyle(.white)
+                                    .font(.subheadline)
+                                    .fontWeight(.medium)
+                            }
+                        }
+                        .padding()
+                    }
+                    .padding(.horizontal, 30)
+                    
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundStyle(Color.black.opacity(0.2))
+                        .padding(.horizontal, 20)
+                    
+                    
+                    Text("Other Courses")
+                        .font(.headline)
+                        .padding(.horizontal, 20)
+                    
+                    ZStack(alignment: .bottomLeading) {
+                        Image("MeditationThree")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 350, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .brightness(-0.1)
+                        
                         VStack(alignment: .leading, spacing: 5) {
-                            Text("Meditation Progress")
+                            Text("Intermediate")
                                 .foregroundStyle(.white)
-                                .font(.system(size: 25))
+                                .font(.title)
                                 .fontWeight(.semibold)
-                            Text("9 Exercise Left")
-                                .foregroundStyle(.secondary)
-                                .font(.system(size: 15))
-                        }
-                        Spacer()
-                        ZStack {
-                            Circle()
-                                .trim(from: 0, to: 0.6)
-                                .rotation(Angle(degrees: -90))
-                                .stroke(Color.white, lineWidth: 5)
-                                .frame(width: 60, height: 60)
                             
-                            Text("60%")
+                            Text("Enhance your meditation skills")
                                 .foregroundStyle(.white)
-                                .font(.headline)
+                                .font(.subheadline)
+                                .fontWeight(.medium)
                         }
+                        .padding()
                     }
-                    .padding(.horizontal, 15)
+                    .padding(.horizontal, 30)
                 }
-                .padding(.horizontal, 20)
-                
-                Text("Current Course")
-                    .font(.headline)
-                    .padding(.horizontal, 20)
                 
                 ZStack(alignment: .bottomLeading) {
-                    Image("MeditationTwo")
+                    Image("MeditationFour")
                         .resizable()
                         .scaledToFill()
                         .frame(width: 350, height: 150)
@@ -65,78 +127,26 @@ struct MeditationPage: View {
                         .brightness(-0.1)
                     
                     VStack(alignment: .leading, spacing: 5) {
-                        Text("Beginner")
-                            .foregroundStyle(.white)
-                            .font(.title)
-                            .fontWeight(.semibold)
-                    
-                        
-                        Text("Take your first steps in meditation")
-                            .foregroundStyle(.white)
-                            .font(.subheadline)
-                            .fontWeight(.medium)
-                    }
-                    .padding()
-                }
-                .padding(.horizontal, 30)
-                
-                
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundStyle(Color.black.opacity(0.2))
-                    .padding(.horizontal, 20)
-                
-                
-                Text("Other Courses")
-                    .font(.headline)
-                    .padding(.horizontal, 20)
-                
-                ZStack(alignment: .bottomLeading) {
-                    Image("MeditationThree")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 350, height: 150)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .brightness(-0.1)
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Intermediate")
+                        Text("Advanced")
                             .foregroundStyle(.white)
                             .font(.title)
                             .fontWeight(.semibold)
                         
-                        Text("Enhance your meditation skills")
+                        Text("Reach absolute inner peace")
                             .foregroundStyle(.white)
                             .font(.subheadline)
                             .fontWeight(.medium)
                     }
+                    
                     .padding()
                 }
+            }
                 .padding(.horizontal, 30)
             }
-            
-            ZStack(alignment: .bottomLeading) {
-                Image("MeditationFour")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 350, height: 150)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .brightness(-0.1)
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Advanced")
-                        .foregroundStyle(.white)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                    
-                    Text("Reach absolute inner piece")
-                        .foregroundStyle(.white)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
-                }
-                .padding()
-            }
-            .padding(.horizontal, 30)
+        }
+    private var beginnerMeditations: [Meditation] {
+        (1...10).compactMap { step in
+        allMeditations["\(step)"]
         }
     }
 }
