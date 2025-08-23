c021d31fa342a4870a9041fd31b239e05af56844
Luka Managadze <managaluka0@gmail.com>
2025-08-23 17:23:26 +0400

    Adding DateFormatter for realtime date

---
+++ HabbitTracker/BeginnerPage.swift
@@ -0,0 +1,18 @@
+//
+//  BeginnerPage.swift
+//  HabbitTracker
+//
+//  Created by Luka Managadze on 8/21/25.
+//
+
+import SwiftUI
+
+struct BeginnerPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeginnerPage()
+}
